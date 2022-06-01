b49dc815b2a212d5c7882f7c44598a0988477086
Eric Kim <cyon256@icloud.com>
2022-06-01 16:09:45 +0900

    restart

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
-import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>hello</BrowserRouter>
   </RecoilRoot>
 );
 
